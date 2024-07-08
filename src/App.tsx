641ce3ce3cd02d096b7a5debbe7f5d2820f5f1d6
Thiago <thiago_moreira22@hotmail.com>
2024-07-08 17:37:57 -0300

    chore: Refactor PostComments component and add tests for adding comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import Post from "./components/Post";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://images.tcdn.com.br/img/img_prod/460977/batman_vs_superman_batmobile_model_kit_dtc_13129_2_20201211162227.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
